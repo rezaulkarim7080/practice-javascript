fc059e780ddce1e9e45cf9d2dc487f194ceec17a
Rezaul Karim <82644789+rezaulkarim7080@users.noreply.github.com>
2022-08-02 18:41:46 +0600

    2nd smg

---
+++ circle.js
@@ -0,0 +1,12 @@
+
+
+const area = r=> 3.14*r*r;
+const circumference = r => 2*3.14*r;
+
+const message = "hello world";
+
+exports.area = area;
+exports.circumference = circumference;
+exports.msg = message;
+
+//console.log(module);
