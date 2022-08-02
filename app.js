9ddcef20d597d83ce71c0eeeb02575682baa0445
Rezaul Karim <82644789+rezaulkarim7080@users.noreply.github.com>
2022-08-02 18:57:05 +0600

    arrow function

--- app.js
+++ app.js
@@ -1,4 +1,76 @@
 
-const circle = require("./circle");
+//const circle = require("./circle");
 
-console.log(circle.area(5));
+//console.log(circle.area(5));
+
+// var a= 34;
+// console.log(a);
+// var a = "rexza";
+// console.log(a);
+
+// let  a= 34;
+// console.log(a);
+// let a = "rexza";
+// console.log(a);
+
+function hello () {
+    console.log("Hello world");
+}
+hello();
+///// Arrow function ///
+let say = ()=>{
+ console.log(" i am arrow function");
+
+}
+say();
+
+let Name_Age = (name,age)=>{
+    console.log(name,age);
+   
+   }
+   Name_Age("reza",34);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
