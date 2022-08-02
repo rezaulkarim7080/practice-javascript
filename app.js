dc4812c42f9d0383a016da665cd9d7e3c429523b
Rezaul Karim <82644789+rezaulkarim7080@users.noreply.github.com>
2022-08-02 19:22:20 +0600

    class topic

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //const circle = require("./circle");
 
 //console.log(circle.area(5));
@@ -13,64 +12,39 @@
 // let a = "rexza";
 // console.log(a);
 
-function hello () {
-    console.log("Hello world");
-}
-hello();
+// function hello () {
+//     console.log("Hello world");
+// }
+// hello();
 ///// Arrow function ///
-let say = ()=>{
- console.log(" i am arrow function");
+// let say = ()=>{
+//  console.log(" i am arrow function");
 
+// }
+// say();
+
+// let Name_Age = (name,age)=>{
+//     console.log(name,age);
+
+//    }
+//    Name_Age("reza",34);
+// let name = name => name;
+// console.log("karim");
+
+class Vehicles {
+  var = "yes";
+  constructor(color, pass) {
+    this.color = color;
+    this.passenger = pass;
+    this.bus = "Car";
+  }
+  say() {
+    console.log(this.color);
+  }
 }
-say();
 
-let Name_Age = (name,age)=>{
-    console.log(name,age);
-   
-   }
-   Name_Age("reza",34);
+let ocj1 = new Vehicles("red", 4);
+let ocj2 = new Vehicles("green", 6);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(ocj1);
+console.log(ocj2);
