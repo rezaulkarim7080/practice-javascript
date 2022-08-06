04994e5da4591ec9188933b95b8511fd46b05294
Rezaul Karim <82644789+rezaulkarim7080@users.noreply.github.com>
2022-08-06 23:13:26 +0600

    EVENT MOPDULE

--- 2/built in module/app.js
+++ 2/built in module/app.js
@@ -8,16 +8,42 @@
 
 /////////////Asyncronas process ////////
 
-const fs = require("fs");
-fs.readFile("./files/input.txt", "utf-8", (error, text) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(text);
-    fs.writeFile("./files/output2.txt", text, "utf-8", (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
-  }
+// const fs = require("fs");
+// fs.readFile("./files/input.txt", "utf-8", (error, text) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(text);
+//     fs.writeFile("./files/output2.txt", text, "utf-8", (error) => {
+//       if (error) {
+//         console.log(error);
+//       }
+//     });
+//   }
+// });
+
+/// EVENT MOPDULE ///
+
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter();
+
+// emitter.on("event1", () => {
+//   console.log("Event 1 Callad");
+// });
+// emitter.on("event2", (e) => {
+//   console.log("Event 2 Callad", e); //// eta call hobe
+// });
+
+// emitter.emit("event2", { name: "karim" });
+
+/////////// transfer object one file to another //////
+
+const MyEvent = require("./myEvant");
+
+const myEvant = new MyEvent();
+
+myEvant.on("event1", () => {
+  console.log("Event 1 Triggerred");
 });
+
+myEvant.function1();
